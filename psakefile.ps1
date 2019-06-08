15ceafb96e18a72358a2950c06345de3ecfda722
Jason Archer <jasonmarcher@gmail.com>
2019-06-08 14:14:27 -0700

    Add task to build help

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,5 +1,6 @@
 Properties {
     $SrcDirectory = "$PSScriptRoot/src/ps1"
+    $HelpDirectory = "$PSScriptRoot/docs"
     $OutputDirectory = "$PSScriptRoot/build"
     $ReportDirectory = "$PSSCriptRoot/reports"
     $DeployDirectory = "$HOME/Documents/WindowsPowerShell/Modules/poco"
@@ -13,7 +14,16 @@ Task 'clean' {
     }
 }
 
-Task 'build' -Depends clean {
+Task 'docs' {
+    New-Item $OutputDirectory -ItemType Directory -ErrorAction SilentlyContinue > $null
+
+    New-ExternalHelp -Path $HelpDirectory -OutputPath $OutputDirectory
+
+    ## Copy about topics
+    Copy-Item "$HelpDirectory/*" -Include "about_*.txt" -Destination $OutputDirectory -Force
+}
+
+Task 'assemble' -Depends clean {
     New-Item $OutputDirectory -ItemType Directory -ErrorAction SilentlyContinue > $null
 
     ## Copy manifest
@@ -26,6 +36,8 @@ Task 'build' -Depends clean {
     }
     Set-Content "$OutputDirectory/poco.psm1" -Value $ModuleContent -Encoding UTF8 -Force
 }
+
+Task 'build' -Depends assemble, docs
 
 Task 'deploy' -Depends build {
     if (Test-Path $DeployDirectory) {
@@ -47,3 +59,10 @@ Task 'checkStyle' -Depends build {
     $Results | Where-Object Severity -eq "Error" | Format-Table | Out-String | Write-Host -ForegroundColor Red
     $Results | Select-Object RuleName,Severity,Line,Column,ScriptName,Message | ConvertTo-Csv -NoTypeInformation | Set-Content "$ReportDirectory/checkstyle.csv" -Force
 }
+
+# Task 'test' {
+#     New-Item $ReportDirectory -ItemType Directory -ErrorAction SilentlyContinue > $null
+
+#     Invoke-Pester `
+#         -OutputFile "$ReportDirectory/tests_unit.xml" -CodeCoverage "$SrcDirectory/*.ps1" -CodeCoverageOutputFile "$ReportDirectory/coverage.xml" 
+# }
